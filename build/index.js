2fcb4b4b8ae801be508146bcb6c5b81a3c8c6d58
nokonoko2525 <c1407448@st.kanazawa-it.ac.jp>
2024-11-07 19:28:14 +0900

    リアクション機能一旦完成

--- build/index.js
+++ build/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 require("dotenv/config");
+const node_emoji_1 = require("node-emoji");
 const client = new discord_js_1.Client({
     intents: [discord_js_1.GatewayIntentBits.Guilds, discord_js_1.GatewayIntentBits.GuildMessages, discord_js_1.GatewayIntentBits.MessageContent]
 });
@@ -11,7 +12,8 @@ client.once('ready', () => {
 client.on('messageCreate', (message) => {
     if (message.author.bot)
         return;
+    const emoji = (0, node_emoji_1.get)(':smile:') ?? '';
     message.react('🤔');
-    message.react('👍');
+    message.react(emoji);
 });
 client.login(process.env.DISCORD_TOKEN);
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
 import { Client, GatewayIntentBits, Message } from 'discord.js';
 import 'dotenv/config';
+import { get } from 'node-emoji';
 
+//botにほしい情報を付与
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
+//ボットを起動したときに行われる処理
 client.once('ready', () => {
   console.log(`Logged in as ${client.user?.tag}`);
 });
 
+//新しいメッセージが送られた時に行われる処理
 client.on('messageCreate', (message: Message) => {
   // ボットが自分自身に返信しないようにする
   if (message.author.bot) return;
 
-  // 特定のメッセージに返信
+  const emoji = get(':smile:') ?? ''
+  // メッセージに返信
   message.react('🤔');
-  message.react('👍');
+  message.react(emoji);
 
 });
 
