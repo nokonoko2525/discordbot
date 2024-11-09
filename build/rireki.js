84909259eb0a25a17557ba3e37517325b5b929bf
nokonoko2525 <c1407448@st.kanazawa-it.ac.jp>
2024-11-09 20:37:28 +0900

    リアクションをつけたときにログに出力される

---
+++ build/rireki.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const client = new discord_js_1.Client({
+    intents: [discord_js_1.GatewayIntentBits.Guilds, discord_js_1.GatewayIntentBits.GuildMessages, discord_js_1.GatewayIntentBits.GuildMessageReactions],
+    partials: [discord_js_1.Partials.Message, discord_js_1.Partials.Channel, discord_js_1.Partials.Reaction],
+});
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}`);
+});
+client.on(discord_js_1.Events.MessageReactionAdd, async (reaction, user) => {
+    if (reaction.partial) {
+        try {
+            await reaction.fetch();
+        }
+        catch (error) {
+            console.error('Something went wrong when fetching the message:', error);
+            return;
+        }
+    }
+    console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+    console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+});
+client.login(process.env.DISCORD_TOKEN);
---
+++ src/rireki.ts
@@ -0,0 +1,36 @@
+import { Client, GatewayIntentBits ,Partials, Events} from 'discord.js';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
+	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+});
+
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user?.tag}`);
+});
+
+client.on(Events.MessageReactionAdd, async (reaction, user) => {
+	// When a reaction is received, check if the structure is partial
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message:', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+	}
+
+	// Now the message has been cached and is fully available
+	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+	// The reaction is now also fully available and the properties will be reflected accurately:
+	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+});
+
+ client.login(process.env.DISCORD_TOKEN);
